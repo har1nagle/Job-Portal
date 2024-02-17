cda3de9c72c01fbd74ef2dfb6d69163d9b1c0ae5
har1nagle <hnagle.0@yahoo.com>
2024-02-17 23:58:39 +0530

    update config dbconnection error file make route file

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ import dotenv from "dotenv"
 import cors from "cors"
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
+import userRouter from "./routes/userRouter.js";
+import applicationRouter from "./routes/applicationRouter.js";
+import jobRouter from "./routes/jobRouter.js";
+import {dbConnection} from './database/dbConnection.js'
+
 
 
 
@@ -24,5 +29,13 @@ app.use(fileUpload({
   useTempFiles: true,
   tempFileDir: "/tmp/",
 }));
+
+
+app.use('/api/v1/user', userRouter);
+app.use("/api/v1/application", applicationRouter);
+app.use("/api/v1/job", jobRouter);
+
+dbConnection();
+
 
 export default app;
