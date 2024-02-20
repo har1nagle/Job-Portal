4f797e892318c53ebb1f341cb7641aa3f9bf1900
har1nagle <hnagle.0@yahoo.com>
2024-02-20 15:29:51 +0530

    frontend started from done basic components

---
+++ frontend/src/components/Home/PopularCampanies.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PopularCampanies = () => {
+  return (
+    <div>PopularCampanies</div>
+  )
+}
+
+export default PopularCampanies
--- middlewares/catchAsyncError.js
+++
@@ -1,5 +0,0 @@
-export const catchAsyncError = (theFunction) => {
-  return (req, res, next) => {
-    Promise.resolve(theFunction(req, res, next)).catch(next);
-  };
-};
