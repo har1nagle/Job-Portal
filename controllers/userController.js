4ac960edc6ed4b7a8ec0ee1004741b89573fa42e
har1nagle <hnagle.0@yahoo.com>
2024-02-20 12:51:17 +0530

    updated user backend done

--- controllers/userController.js
+++ controllers/userController.js
@@ -57,3 +57,11 @@ export const logout = catchAsyncError(async (req, res, next) => {
     message: "User logged out successfully!",
   });
 });
+
+export const getUser = catchAsyncError( (req, res, next) => {
+  const user = req.user;
+  res.status(200).json({
+    success: true,
+    user,
+  })
+})
